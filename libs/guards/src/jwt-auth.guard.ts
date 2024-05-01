fe2b1e07f181258afd6d79c304f8cc0084ea062d
Santosh Mahat <saraswotimc@ims.local>
2024-05-01 09:43:26 +0545

    added jtw auth guard as libs

---
+++ libs/guards/src/jwt-auth.guard.ts
@@ -0,0 +1,38 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    @Inject(JwtService) private readonly jwtService: JwtService,
+    @Inject(ConfigService) private readonly configService: ConfigService
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) throw new UnauthorizedException();
+
+    try {
+      const decodedUser = await this.jwtService.verify(token, {
+        secret: this.configService.get('JWT_SECRET'),
+      });
+      request['user'] = decodedUser;
+    } catch (error) {
+      console.log(error);
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request?.headers?.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
