59940844a657fd59d87032b37529913bc662ed26
Santosh Mahat <saraswotimc@ims.local>
2024-05-04 21:51:20 +0545

    remove unused code

--- apps/api-gateway/src/api-gateway.service.ts
+++ apps/api-gateway/src/api-gateway.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ApiGatewayService {
   getHello(): string {
-    return 'Hello World! Santosh';
+    return 'Hello World!';
   }
 }
